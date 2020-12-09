34bc08c56b54571f8e1bc1d1be9dc42f35e28f1d
Guilherme Santos Souza <guilherme.souza@smlbrasil.com.br>
2020-12-08 21:49:03 -0300

    Primeiro commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace poc_gitworkflow.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            TempData["AppName"] = _configuration["AppName"];
+        }
+    }
+}
